ddc7d0c32a1950b945fac94292cd50a5310d4556
Ilman Sunanuddin <ilmansunannudin2@gmail.com>
2023-05-23 10:08:04 +0700

    jwt auth

---
+++ src/main/java/com/mpedia/ecommerce/security/service/UserDetailsImpl.java
@@ -0,0 +1,89 @@
+package com.mpedia.ecommerce.security.service;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.util.StringUtils;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.mpedia.ecommerce.entity.User;
+
+public class UserDetailsImpl implements UserDetails {
+
+  private String username;
+  private String email;
+  private String name;
+  @JsonIgnore
+  private String password;
+  @JsonIgnore
+  private String roles;
+
+  public UserDetailsImpl(String username, String email, String name, String password, String roles) {
+    this.username = username;
+    this.email = email;
+    this.name = name;
+    this.password = password;
+    this.roles = roles;
+  }
+
+  public static UserDetailsImpl build(User user){
+    return new UserDetailsImpl(user.getId(),
+    user.getEmail(),
+    user.getName(),
+    user.getPassword(),
+    user.getRoles()
+    );
+  }
+
+  @Override
+  public Collection<? extends GrantedAuthority> getAuthorities() {
+    List<GrantedAuthority> authorities = new ArrayList<>();
+    if(StringUtils.hasText(roles)){
+     String[] splits =  roles.replaceAll(" ", "").split(",");
+     for (String string : splits) {
+      authorities.add(new SimpleGrantedAuthority(string));
+     }
+    }
+    return authorities;
+   
+  }
+
+  @Override
+  public String getPassword() {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'getPassword'");
+  }
+
+  @Override
+  public String getUsername() {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'getUsername'");
+  }
+
+  @Override
+  public boolean isAccountNonExpired() {
+    // TODO Auto-generated method stub
+   return true;
+  }
+
+  @Override
+  public boolean isAccountNonLocked() {
+   return true;
+  }
+
+  @Override
+  public boolean isCredentialsNonExpired() {
+   return true;
+  }
+
+  @Override
+  public boolean isEnabled() {
+  return true;
+  }
+  
+  
+}
